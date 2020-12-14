133dd7e3b55b337fc08fe595b3439429608495d2
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2020-12-14 13:28:02 -0800

    commit02

--- js/app.js
+++ js/app.js
@@ -50,23 +50,96 @@ $('#page2').on('click', function () {
   $(page2).show();
 
 });
-function sortByTitle(images) {
-  images.sort(function (a, b) {
-    let nameA = a.toUpperCase();
-    let nameB = b.toUpperCase();
-    if (nameA < nameB) {
-      return -1;
 
-    } if (nameA > nameB) {
-      return 1;
-    }
+const sortByHorns = (a, b) => {
+  if (a.horns < b.horns) {
+    return -1;
+
+  } else if (a.horns > b.horns) {
+    return 1;
+  } else {
     return 0;
+  }
+};
+const sortByTitle = (a, b) => {
+  if (a.title.toLowerCase() < b.title.toLowerCase()) {
+    return -1;
 
-  });
-}
-$('#keyword-filter').on('change', (e) => {
- let images = $('img');
- 
-  
+  } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
 
-})
+
+$('.option').on('click', (e) => {
+  let chooseSort = e.target.textContent;
+  if (chooseSort === 'Sort by Horns') {
+   Images.sort(Horns);
+  }
+  if (chooseSort === 'Sort by Title') {
+    Images.sort(Title);
+  }
+  $('section').empty();
+  // Images.forEach((image) => image.renderWithMustache());
+  // $('.All').hide();
+  // $(`.${choice}`).show();
+  // if (pageShown === 1) {
+  //   $('#page2').hide();
+
+  // } else {
+  //   $('#page1').hide();
+  // }
+});
+
+
+
+
+// function sortByTitle(images) {
+//   images.sort(function (a, b) {
+//     let nameA = a.toUpperCase();
+//     let nameB = b.toUpperCase();
+//     if (nameA < nameB) {
+//       return -1;
+
+//     } if (nameA > nameB) {
+//       return 1;
+//     }
+//     return 0;
+
+//   });
+// }
+// // $('#keyword-filter').on('change', (e) => {
+// //  let images = $('img');
+
+
+// // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
